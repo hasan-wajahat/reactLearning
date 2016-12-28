26d31609c4d28d7db51c53e99a186661627d1107
Hasan <hasan_wajahat@hotmail.com>
2016-12-28 13:17:00 +0500

    created a react project for learning

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    databaseURL: 'https://catch-of-the-day-ed385.firebaseio.com/',
+    apiKey: "AIzaSyDbuAIKKtyrY6l44u81RuEuFlv74i5SdUE",
+    authDomain: "catch-of-the-day-ed385.firebaseapp.com",
+});
+
+export default base;
